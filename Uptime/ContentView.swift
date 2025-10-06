695dfd7e41a9ee16896c7ddf050b2a4d823b3cdb
Curtis Netterville <curtis@netterville.com>
2025-10-06 12:46:45 -0500

    Initial commit for checkpoints

---
+++ Uptime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uptime
+//
+//  Created by Curtis Netterville on 10/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uptime/UptimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UptimeApp.swift
+//  Uptime
+//
+//  Created by Curtis Netterville on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct UptimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
